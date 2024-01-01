b7da3d2d3cf7717d937f38a4ad91625d6689fbe8
KhantWaiLin <khantkhant.cm@gmail.com>
2024-01-01 10:49:52 +0630

    go to dashboard page if session doesn't expire

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -17,6 +17,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
+        dd(session());
         return view('auth.login');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.register');
+    if(auth()->check()){
+        return redirect('/dashboard');
+    }
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
